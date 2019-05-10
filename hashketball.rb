810777842e9074cc953b63dd5e96d7c613a5a661
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 20:02:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -76,7 +76,7 @@ def game_hash
           :number => 0,
           :shoe => 16,
           :points => 12,
-          :rebounds => 14,
+          :rebounds => 4,
           :assists => 7,
           :steals => 7,
           :blocks => 15,
