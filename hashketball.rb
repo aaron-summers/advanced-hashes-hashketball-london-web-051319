d8798fbf4d2f5463a09ba068ef2356969c27eb26
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 20:02:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,8 +181,6 @@ def player_stats(name)
     data[:players].each do |player_name, value|
       if player_name == name
         return value
-      # else 
-      #   return nil
       end
     end
   end
